a824ce83745f0ce551041c949fc1457b62191f74
IanD1013 <ian.tung1013@gmail.com>
2024-07-21 22:56:53 +1200

    Add LoginPage

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={"/login"} element={<div>login page</div>} />
+        <Route path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
   );
