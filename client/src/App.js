2dc6135e15e3a702f70504b5c22595baf59ae801
IanD1013 <ian.tung1013@gmail.com>
2024-07-21 22:53:09 +1200

    Add IndexPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Layout from "./Layout";
-import Post from "./Post";
+import IndexPage from "./pages/IndexPage";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Post />} />
+        <Route index element={<IndexPage />} />
         <Route path={"/login"} element={<div>login page</div>} />
       </Route>
     </Routes>
