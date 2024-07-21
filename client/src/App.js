574f5eebfb811c964b11e5b35070031d5f6fd382
IanD1013 <ian.tung1013@gmail.com>
2024-07-21 22:48:29 +1200

    Add Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,15 @@
 import "./App.css";
-import Header from "./Header";
+import Layout from "./Layout";
 import Post from "./Post";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }
-      />
-      <Route
-        path={"/login"}
-        element={
-          <main>
-            <Header />
-            <div>login page</div>
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Post />} />
+        <Route path={"/login"} element={<div>login page</div>} />
+      </Route>
     </Routes>
   );
 }
