c16471806a3c139bbf345d83b8d15769c82337ab
IanD1013 <ian.tung1013@gmail.com>
2024-07-24 20:28:26 +1200

    Add api handlers for registration and login

---
+++ api/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const app = express();
+const jwt = require("jsonwebtoken");
+
+const salt = bcrypt.genSaltSync(10);
+const secret = process.env.SECRET;
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("wrong credentials");
+  }
+});
+
+app.listen(4000);
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(ev) {
+    ev.preventDefault();
+
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.status === 200) {
+      alert("Registration successfully!");
+    } else {
+      alert("Registration failed!");
+    }
+  }
+
   return (
-    <form className="register">
+    <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Register</button>
     </form>
   );
