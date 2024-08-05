4e3e66ecc8268490abf81b2edaeb183c2b9ea581
IanD1013 <ian.tung1013@gmail.com>
2024-08-05 22:38:08 +1200

    Add CreatePost router and page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
-import Layout from "./pages/Layout";
-import IndexPage from "./pages/IndexPage";
 import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import CreatePost from "./pages/CreatePost";
+import IndexPage from "./pages/IndexPage";
+import Layout from "./pages/Layout";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import { UserContext, UserContextProvider } from "./UserContext";
+import { UserContextProvider } from "./UserContext";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
