3efa9a647362595fc6fa9f0fb10bfffb701b23f3
IanD1013 <ian.tung1013@gmail.com>
2024-08-06 09:30:12 +1200

    Receive and parse uploads at the backend

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -15,7 +15,7 @@ export default function CreatePost() {
     data.set("title", title);
     data.set("summary", summary);
     data.set("content", content);
-    data.set("files", files[0]);
+    data.set("file", files[0]);
 
     fetch("http://localhost:4000/post", {
       method: "POST",
