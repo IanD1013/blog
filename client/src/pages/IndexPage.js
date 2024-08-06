ce8976492546bfb5df7667d064940e538382c2d3
IanD1013 <ian.tung1013@gmail.com>
2024-08-06 13:22:54 +1200

    Add redirection to single post page at the frontend

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -12,5 +12,10 @@ export default function IndexPage() {
     });
   }, []);
 
-  return <>{posts.length > 0 && posts.map((post) => <Post {...post} />)}</>;
+  return (
+    <>
+      {posts.length > 0 &&
+        posts.map((post) => <Post key={post._id} {...post} />)}
+    </>
+  );
 }
