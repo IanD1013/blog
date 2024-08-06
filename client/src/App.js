b9bf26d760a6e7ea234589407c17596b57bc6a19
IanD1013 <ian.tung1013@gmail.com>
2024-08-06 14:10:16 +1200

    Single post page skeleton done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import CreatePost from "./pages/CreatePost";
 import IndexPage from "./pages/IndexPage";
 import Layout from "./pages/Layout";
 import LoginPage from "./pages/LoginPage";
+import PostPage from "./pages/PostPage";
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./UserContext";
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
